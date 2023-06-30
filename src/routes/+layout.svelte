a524b8ec068bcb67103913337de8b3797c651d8f
rnba12 <rnba12@yahoo.co.uk>
2023-06-30 21:28:10 +0100

    Feat: home page content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,49 @@
-<nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/projects">Projects</a>
-    <a href="/cv.pdf" target="_blank">CV</a>
-    <a href="/contact">Contact</a>
-</nav>
+<script lang="ts">
+    import '../main.scss'
+    import { page } from '$app/stores';
 
-<slot/>
+    $: path = $page.route.id
+
+</script>
+
+<div class="page">
+    <div class="top-nav">
+        <nav>
+            <a href="/" class:active={path === "/"}>Home</a>
+            <a href="/about" class:active={path === "/about"}>About</a>
+            <a href="/projects" class:active={path === "/projects"}>Projects</a>
+            <a href="/cv.pdf" target="_blank">CV</a>
+            <a href="/contact" class:active={path === "/contact"}>Contact</a>
+            <button>*</button>
+        </nav>
+    </div>
+    
+    <slot/>
+</div>
+
+<style lang="scss">
+    .page {
+        padding: 0 4rem;
+    }
+    .top-nav {
+        height: var(--navbar-height);
+
+        nav {
+            height: 100%;
+            float: right;
+            display: flex;
+            align-items: center;
+            gap: 1.5rem;
+        }
+        a {
+            text-decoration: none;
+            color: black;
+            font-size: 1.2rem;
+            font-weight: 600;
+            &.active, &:hover {
+                color: rgb(0, 115, 255);
+            }
+
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<h1>Home</h1>
+<h1>Hi, I'm Russell 👋</h1>
+
+<h2> I'm a recent grad in Electronic Engineering. I'm currently looking to gain some experience in software engineering/development.</h2>
+
+<h2>Here you can find out a bit about me and some stuff i've worked on.</h2>
