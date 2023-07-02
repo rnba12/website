be6513b2f3248091f3e835a7b47a8aeb073c5f9a
rnba12 <rnba12@yahoo.co.uk>
2023-07-02 19:08:55 +0100

    Feat: add links to project card

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { fade } from "svelte/transition";
+
     export let name: string
     export let description: string
     export let image: string
@@ -9,7 +11,7 @@
 
 <div class="project-card">
     <img src={image} alt={name}>
-    <div class="content">
+    <div class="content" transition:fade>
         <div class="name">{name}</div>
         <div class="description">{description}</div>
         <div class="technologies">
@@ -18,8 +20,13 @@
             {/each}
         </div>
         <div class="links">
-            <a href={link}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6m-7 1l9-9m-5 0h5v5"/></svg></a>
-            <a href={github}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 .297c-6.63 0-12 5.373-12 12c0 5.303 3.438 9.8 8.205 11.385c.6.113.82-.258.82-.577c0-.285-.01-1.04-.015-2.04c-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729c1.205.084 1.838 1.236 1.838 1.236c1.07 1.835 2.809 1.305 3.495.998c.108-.776.417-1.305.76-1.605c-2.665-.3-5.466-1.332-5.466-5.93c0-1.31.465-2.38 1.235-3.22c-.135-.303-.54-1.523.105-3.176c0 0 1.005-.322 3.3 1.23c.96-.267 1.98-.399 3-.405c1.02.006 2.04.138 3 .405c2.28-1.552 3.285-1.23 3.285-1.23c.645 1.653.24 2.873.12 3.176c.765.84 1.23 1.91 1.23 3.22c0 4.61-2.805 5.625-5.475 5.92c.42.36.81 1.096.81 2.22c0 1.606-.015 2.896-.015 3.286c0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg></a>
+            {#if link}     
+                <a href={link}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6m-7 1l9-9m-5 0h5v5"/></svg>{link}</a>
+            {/if}
+            <br>
+            {#if github}     
+                <a href={github}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 .297c-6.63 0-12 5.373-12 12c0 5.303 3.438 9.8 8.205 11.385c.6.113.82-.258.82-.577c0-.285-.01-1.04-.015-2.04c-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729c1.205.084 1.838 1.236 1.838 1.236c1.07 1.835 2.809 1.305 3.495.998c.108-.776.417-1.305.76-1.605c-2.665-.3-5.466-1.332-5.466-5.93c0-1.31.465-2.38 1.235-3.22c-.135-.303-.54-1.523.105-3.176c0 0 1.005-.322 3.3 1.23c.96-.267 1.98-.399 3-.405c1.02.006 2.04.138 3 .405c2.28-1.552 3.285-1.23 3.285-1.23c.645 1.653.24 2.873.12 3.176c.765.84 1.23 1.91 1.23 3.22c0 4.61-2.805 5.625-5.475 5.92c.42.36.81 1.096.81 2.22c0 1.606-.015 2.896-.015 3.286c0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>GitHub</a>
+            {/if}
         </div>
     </div>
 </div>
@@ -28,14 +35,16 @@
     .project-card {
         background: inherit;
         position: relative;
-        border-radius: 15px;
+        border-radius: 28px;
         width: 450px;
         height: 280px;
+        box-sizing: border-box;
         
-        &:hover {
+        &:focus-within, &:hover {
             .content {
-                visibility: visible;
+                opacity: 1;
             }
+            border: 1px solid grey;
         }
     }
 
@@ -46,7 +55,7 @@
         width: 100%;
         height: 100%;
         object-fit: cover;
-        border-radius: 25px;
+        border-radius: 28px;
     }
 
     .content {
@@ -54,30 +63,46 @@
         top: 0;
         left: 0;
         z-index: 3;
-        visibility: hidden;
         width: 100%;
         height: 100%;
         border-radius: inherit;
         box-sizing: border-box;
         padding: 1rem;
         color: black;
-        background-color: rgba(255, 255, 255, 0.95);
+        background-color: rgba(255, 255, 255);
+        display: flex;
+        flex-direction: column;
+        gap: 0.8rem;
+        opacity: 0;
+        transition: all 0.1s;
     }
     .name {
-        font-size: 1.4rem;
+        font-size: 1.3rem;
         font-weight: 600;
+        text-transform: capitalize;
     }
     .description {
-        font-size: 1.1rem;
+        // font-size: 1.3rem;
     }
     .technologies {
+        display: flex;
+        gap: 0.2rem;
+        flex-wrap: wrap;
+        align-items: center;
         span {
-            padding: 0.2rem;
+            padding: 0.1rem 0.3rem;
             border-radius: 25px;
-            background-color: rgb(154, 154, 154);
-            font-weight: 500;
-            color: white;
+            border: 1px solid black;
         }
+    }
+    .links {
+       a {
+        color: black;
+        text-decoration: none;
+        display: inline-flex;
+        align-items: center;
+        gap: 5px;
+       }
     }
    
 </style>
