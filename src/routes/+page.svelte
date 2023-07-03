fbd31c6b92eff1eb1738c1d7cbc88f7abf4d9c4b
rnba12 <rnba12@yahoo.co.uk>
2023-07-03 02:08:24 +0100

    Style: home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
-<h1>Hi, I'm Russell 👋</h1>
+<svelte:head>
+    <title>Russell Ampomah</title>
+</svelte:head>
 
-<h2> I'm a recent grad in Electronic Engineering. I'm currently looking to gain some experience in software engineering/development.</h2>
+<h1>Hi, I'm <span class="name">Russell</span></h1>
+
+<h2> I'm a recent grad in Electronic Engineering. I'm currently looking to gain some experience in software development.</h2>
 
 <h2>Here you can find out a bit about me and some stuff i've worked on.</h2>
+
+<style>
+    .name {
+        /* color: rgb(0, 115, 255); */
+        font-weight: 700;
+    }
+    h1 {
+        font-weight: 600;
+    }
+    h2 {
+        font-weight: 500; 
+    }
+</style>
