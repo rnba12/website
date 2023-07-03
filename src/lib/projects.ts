f820565a5b93020ec6235b4f826341df73d0ea2c
rnba12 <rnba12@yahoo.co.uk>
2023-07-03 03:12:39 +0100

    Chore: update project info

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -9,32 +9,32 @@ const projectList : {
     {
         name: "Profile Maker",
         description: "A page for displaying coding projects",
-        link: "https://profile-maker.vercel.app/",
+        link: "https://profile-maker.vercel.app",
         github: "https://github.com/profile-maker",
         image: "src/lib/assets/profilemaker.png",
         technologies: ["Svelte", "Prisma", "PostgreSQL", "Sass", "Supabase"]
     },
     {
         name: "Synonymz",
-        description: "A word game",
+        description: "A simple game where you guess synonyms",
         link: "https://synonymz.netlify.app/",
-        github: null,
+        github: "https://github.com/rnba12/synonymz",
         image: "src/lib/assets/synonymz.png",
-        technologies: ["React"]
+        technologies: ["React", "JavaScript"]
     },
     {
         name: "Early Bird",
-        description: "Multiplayer quiz game",
+        description: "A Multiplayer quiz game",
         link: "https://voluble-zabaione-2be1b6.netlify.app/",
-        github: null,
+        github: "https://github.com/mizupp/thewisequacks",
         image: "src/lib/assets/earlybird.png",
-        technologies: ["React", "MongoDB", "Tailwind CSS"]
+        technologies: ["React", "MongoDB", "TailwindCSS", "Socket.io"]
     },
     {
         name: "House Your Plant",
         description: "House plant health and tracking app",
         link: "https://main--fanciful-tarsier-29e66b.netlify.app/",
-        github: null,
+        github: "https://github.com/mizupp/plantastic-four",
         image: "src/lib/assets/houseyourplant.png",
         technologies: ["Python", "Flask", "React", "PyTorch", "MongoDB"]
     },
@@ -42,9 +42,9 @@ const projectList : {
         name: "Habitual",
         description: "Habit tracking web application",
         link: "https://relaxed-speculoos-a7d774.netlify.app/client/login.html",
-        github: null,
+        github: "https://github.com/JLP2000/habitual-client",
         image: "src/lib/assets/habitual.png",
-        technologies: ["HTML", "CSS", "JavaScript", "PostgreSQL"]
+        technologies: ["HTML5", "CSS3", "JavaScript", "PostgreSQL"]
     },
     {
         name: "See It Say It",
@@ -52,7 +52,7 @@ const projectList : {
         link: "https://coruscating-dasik-fbf1c6.netlify.app/html/home.html",
         github: "https://github.com/adamminchella/see-it-say-it",
         image: "/src/lib/assets/seeitsayit.png",
-        technologies: ["HTML", "CSS", "JavaScript"]
+        technologies: ["HTML5", "CSS3", "JavaScript"]
     },
     {
         name: "Github Tracker",
@@ -60,15 +60,15 @@ const projectList : {
         link: null,
         github: "https://github.com/shedp/GithubTrackerReact",
         image: "src/lib/assets/githubtracker.png",
-        technologies: []
+        technologies: ["React", "JavaScript", "Redux"]
     },
     {
-        name: "Post It",
+        name: "Post_It",
         description: "Anonymous note taking app. Clone of telegra.ph",
         link: null,
         github: "https://github.com/rnba12/post_it",
         image: "src/lib/assets/postit.png",
-        technologies: ["HTML", "CSS", "JavaScript", "MongoDB", "Jest"]
+        technologies: ["HTML5", "CSS3", "JavaScript", "MongoDB", "Jest"]
     },
 ]
 
