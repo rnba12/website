af6c9f27a948ecbfea16fd0726cfb4d102ba96ba
rnba12 <rnba12@yahoo.co.uk>
2023-07-03 02:07:59 +0100

    Style: about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,9 +2,13 @@
     import data from '../../lib/about.json'
 </script>
 
-<section>
-    <p>
-        I graduated last year from the University of York in Electronic Engineering. After completing my degree, I decided I wanted to move into software engineering. I took a course at La Fosse Academy in London to improve my knowledge and work on some <a href="/projects">projects</a>.
+<svelte:head>
+    <title>About | Russell Ampomah</title>
+</svelte:head>
+
+<section class="bio">
+    <p style="margin-top: 0;">
+        I graduated last year from the University of York in Electronic Engineering. After completing my degree, I decided I wanted to move into software engineering. I took a course at La Fosse Academy in London to improve my knowledge and work on some projects.
      </p><p>
         I can work across the development stack, and I'm always looking to learn new tools and technologies. At the moment i'm looking to gain some commercial experience.
         </p>
@@ -13,32 +17,41 @@
     </p>
 </section>
 
-<h3>Skills</h3>
 
-<section class="skills">
-    <div class="list">
-        <div class="header">Languages</div>
-        {#each data.languages as lang}
-            <span>{lang}</span>
-        {/each}
+<section>
+    <h4 style="margin-bottom: 1rem;">Skills</h4>
+
+    <div class="skills">
+        <div class="list">
+            <div class="header">Languages</div>
+            {#each data.languages as lang}
+                <span>{lang}</span>
+            {/each}
+        </div>
+        <div class="list">
+            <div class="header">Framworks</div>
+            {#each data.frameworks as lang}
+                <span>{lang}</span>
+            {/each}
+        </div>
+        <div class="list">
+            <div class="header">Tools</div>
+            {#each data.tools as lang}
+                <span>{lang}</span>
+            {/each}
+        </div>
     </div>
-    <div class="list">
-        <div class="header">Framworks</div>
-        {#each data.frameworks as lang}
-            <span>{lang}</span>
-        {/each}
-    </div>
-    <div class="list">
-        <div class="header">Tools</div>
-        {#each data.tools as lang}
-            <span>{lang}</span>
-        {/each}
-    </div>
+    <a class="cv-link" href="/cv.pdf">
+        <span>View my CV</span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="19.2" height="19.2" viewBox="0 0 256 256"><g transform="rotate(90 128 128)"><path fill="currentColor" d="M208.49 120.49a12 12 0 0 1-17 0L140 69v147a12 12 0 0 1-24 0V69l-51.51 51.49a12 12 0 0 1-17-17l72-72a12 12 0 0 1 17 0l72 72a12 12 0 0 1 0 17Z"/></g></svg>
+    </a>
 </section>
 
-<a class="cv-link" href="/cv.pdf">View my CV</a>
 
 <style lang="scss">
+    .bio {
+        font-size: 1.1rem;
+    }
     .list {
         display: flex;
         flex-direction: column;
@@ -52,8 +65,17 @@
         font-weight: 700;
     }
     .cv-link {
-        display: block;
+        display: inline-flex;
+        align-items: center;
+        gap: 5px;
+        font-size: 1.2rem;
+        margin-top: 2rem;
+        color: inherit;
         text-decoration: none;
-        margin: 1rem 0;
+        font-weight: 600;
+        transition: all 0.1s;
+        &:hover {
+            color: rgb(0, 115, 255);
+        }
     }
 </style>
