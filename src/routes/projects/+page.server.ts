6b40048d9054da4a296bd6e13a919750cab9556c
rnba12 <rnba12@yahoo.co.uk>
2025-08-30 16:16:32 +0100

    chore: v2 init

--- src/routes/projects/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types';
-import projectList from '$lib/projects';
-
-export const load = (async () => {
-    return {
-        projectList
-    };
-}) satisfies PageServerLoad;
